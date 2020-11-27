65e46730377766a10ce6f6a2be01cd7015b63ffc
Paweł <pawel.nieczuja@onet.eu>
2020-11-27 13:45:25 +0100

    Create structure

---
+++ front/books/src/App.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+import './App.css';
+
+function App() {
+  const [data, setData] = useState(1);
+
+  return (
+    <div className="App">
+      <h1>Books Storage</h1>
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ front/books/src/store.js
@@ -0,0 +1,10 @@
+import { createStore } from 'redux';
+
+const bookReducer = (state, action) => {
+
+  return {
+
+  }
+}
+
+export const store = createStore(bookReducer);
